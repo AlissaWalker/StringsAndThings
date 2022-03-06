c7e6ccd142a292e57bd11ff35b0ac11ecaa7680f
Alissa <alissa@Zipcoders-MBP-3.hsd1.de.comcast.net>
2022-03-05 20:44:20 -0500

    Equal Number Test

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -13,15 +13,17 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
+    //i = index marker
+    public Integer countYZ(String input) {
         String[] splitWords = input.split(" ");
-        Integer counter= 0;
-        for (int i = 0; i <splitWords.length ; i++) {
-            if (splitWords[i].substring(splitWords[i].length() - 1).toLowerCase(Locale.ROOT).equals("y") || splitWords[i].substring(splitWords[i].length() - 1).toLowerCase(Locale.ROOT).equals("z")){
-                counter ++;
+        Integer counter = 0;
+        for (int i = 0; i < splitWords.length; i++) {
+            if (splitWords[i].substring(splitWords[i].length() - 1).toLowerCase(Locale.ROOT).equals("y")
+                    || splitWords[i].substring(splitWords[i].length() - 1).toLowerCase(Locale.ROOT).equals("z")) {
+                counter++;
             }
         }
         return counter;
@@ -31,15 +33,15 @@ public class StringsAndThings {
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
+     * removeString("Hello there", "e") //  Should return "Hllo thr"
+     * removeString("Hello there", "x") // Should return "Hello there"
      */
     //created a new base and set it equal. Replaces the first substring of this string that matches the given regular expression with the given replacement.
-    public String removeString(String base, String remove){
+    public String removeString(String base, String remove) {
 
-        String newBase = base.replace(remove,"");
+        String newBase = base.replace(remove, "");
 
         return newBase;
     }
@@ -47,14 +49,39 @@ public class StringsAndThings {
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+     * containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+     * containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    //
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        //Boolean newInput = Boolean.parseBoolean(input);
+        //get count for's is and nots
+        //   input ==
+        //return ;
+        //char sample  = input.charAt(1)
+        //.length() is the size of the string
+        int isCounter = 0;
+        int notCounter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (i+1 <input.length()&& input.charAt(i) == 'i' && input.charAt(i + 1) == 's' ) { // this is not i+1 it looks at 1 char then goes to the next if its true
+                isCounter++;
+            } else {
+                if (i +2< input.length() && input.charAt(i) == 'n' && input.charAt(i + 1) == 'o' && input.charAt(i + 2) == 't') { //i +2 stops it from running at the end of the string  index
+                    notCounter++;
+                }
+            }
+           // return
+            //[ this is not
+            //  012345678910
+        }
+        return (isCounter == notCounter);
     }
+
+
+
+
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
